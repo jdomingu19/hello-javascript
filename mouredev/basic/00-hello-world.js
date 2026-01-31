aa576f8978dea9712e33a9e57f8adb1da7cb9d27
Jesús Domínguez <jesusdominguez2004@gmail.com>
2026-01-31 10:52:42 -0500

    feat(hello-javascript): add mouredev learning folder
    - Add mouredev/basic/00-hello-world.js with console examples
    - Add mouredev/guia_fundamentos_javascript_mouredevpro.pdf as reference guide
    - Include initial structure for course-based learning resources

---
+++ mouredev/basic/00-hello-world.js
@@ -0,0 +1,34 @@
+// JavaScript Course from Scratch @mouredev
+
+// This is a one line comment
+
+/*
+This is 
+a comment 
+that uses 
+multiple lines
+*/
+
+// Output a message to the console
+console.log("Hello, JavaScript!");
+console.log('Hello, JavaScript!');
+console.log(`Hello, JavaScript!`);
+
+// console.log("Hello, JavaScript!");
+// console.log('Hello, JavaScript!');
+// console.log(`Hello, JavaScript!`);
+
+/*
+console.log("Hello, JavaScript!");
+console.log('Hello, JavaScript!');
+console.log(`Hello, JavaScript!`);
+*/
+
+console.log('5');
+console.log(5);
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
