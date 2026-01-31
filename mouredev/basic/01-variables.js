322b72c30cecde6dd94a8ec77b5f3a861e737840
Jesús Domínguez <jesusdominguez2004@gmail.com>
2026-01-31 16:12:17 -0500

    feat(hello-javascript): add variables example
    - Add mouredev/basic/01-variables.js with var, let, and const usage
    - Include console output examples for string reassignment and constants
    - Provide initial practice file for JavaScript course from scratch

---
+++ mouredev/basic/01-variables.js
@@ -0,0 +1,23 @@
+// JavaScript Course from Scratch @mouredev
+
+// 1. var
+var myString = "Hello, JavaScript!";
+console.log(myString); // Hello, JavaScript!
+
+myString = "This is a String";
+console.log(myString); // This is a String
+
+// 2. let
+let myString2 = "Hello, Node.js!";
+console.log(myString2); // Hello, Node.js!
+
+myString2 = "This is also a String";
+console.log(myString2); // This is also a String
+
+// 3. const
+const myString3 = "Hello, React!";
+console.log(myString3); // Hello, React!
+
+// TypeError: Assignment to constant variable.
+// myString3 = "This is another String";
+// console.log(myString3); // This is another String
